ae0fb058575de99ff85dd47bbe6d7fb205ca9cb2
𝚖𝚒𝚜𝚑𝚛𝚊𝚐𝚊 <gagan@minbox.in>
2022-10-18 21:43:17 -0700

    connect level nodes using marker

---
+++ src/main/kotlin/in/minbox/trees/TreeUp.kt
@@ -0,0 +1,41 @@
+package `in`.minbox.trees
+
+object TreeUp {
+    data class TreeNode(val value: Int, val children: MutableList<TreeNode>, var nextNode: TreeNode?) {
+        var isLeaf: Boolean = false
+
+        fun addChild(child: TreeNode) {
+            children.add(child)
+        }
+    }
+
+    fun connectSameLevelNodes(root: TreeNode) {
+        assert(root != null) { "root cannot be null" }
+        val marker = TreeNode(-1, mutableListOf(), null)
+
+        val queue = java.util.ArrayDeque<TreeNode>()
+        queue.add(root)
+        queue.add(marker)
+
+        while (queue.isNotEmpty()) {
+            val node = queue.poll()
+            if (!node.equals(marker)) {
+                val next = queue.peek()
+                node.nextNode = if (!next.equals(marker)) next else null
+
+                if (node.children.isNotEmpty()) {
+                    for (n in node.children) {
+                        queue.add(n)
+                    }
+
+                    // The other way is to connect the nodes here and push them to the queue
+                    // that will help in removing the marker node, but doesn't improve the code readability
+                }
+            }
+
+            if (queue.isNotEmpty()) {
+                queue.add(marker)
+            }
+        }
+    }
+}
--- src/test/kotlin/in/minbox/trees/TreeTest.kt
+++ src/test/kotlin/in/minbox/trees/TreeTest.kt
@@ -16,4 +16,20 @@ class TreeTest {
         val tree: Tree = Tree.buildTree(intArrayOf(1,5,5,2,2,-1,3))
         tree.convertToLL()
     }
+
+
+    @Test
+    fun connectNodesInSameLevel() {
+        val treeNode = TreeUp.TreeNode(1, mutableListOf(), null)
+        val third = TreeUp.TreeNode(3, mutableListOf(), null)
+        treeNode.addChild(TreeUp.TreeNode(2, mutableListOf(), null))
+        treeNode.addChild(third)
+        treeNode.addChild(TreeUp.TreeNode(4, mutableListOf(), null))
+
+        third.addChild(TreeUp.TreeNode(5, mutableListOf(), null))
+        third.addChild(TreeUp.TreeNode(6, mutableListOf(), null))
+        third.addChild(TreeUp.TreeNode(7, mutableListOf(), null))
+
+        TreeUp.connectSameLevelNodes(treeNode)
+    }
 }
