50a9e0d7a5a1cf4432931d8598d50a15bca7d029
ᨏ <gagan.mishra@gmail.com>
2024-02-15 13:57:17 -0800

    updated matrix rotation

--- src/main/java/in/minbox/math/Math.java
+++ src/main/java/in/minbox/math/Math.java
@@ -55,4 +55,25 @@ public class Math {
         return grid[grid.length - 1][grid[0].length - 1];
     }
 
+
+    // leetcode: 48 https://leetcode.com/problems/rotate-image/description/
+    public void rotate2(int[][] matrix) {
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = i; j < matrix[0].length; j++){
+                int temp ;
+                temp =  matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for(int i = 0; i < matrix.length; i++){
+            for(int j =0; j < matrix.length / 2; j++){
+                int temp = 0 ;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
+        }
+    }
 }
--- src/test/java/in/minbox/math/MathTest.java
+++ src/test/java/in/minbox/math/MathTest.java
@@ -20,6 +20,17 @@ class MathTest {
     }
 
     @Test
+    void rotate2() {
+        // int[][] matrix = new int[][]{{5,1,9,11},{2,4,8,10},{13,3,6,7}, {15,14,12,16}};
+        //int[][] matrix = new int[][]{{4,8},{3,6}};
+        int[][] matrix = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+        new Math().rotate2(matrix);
+        for (int[] row : matrix){
+            System.out.println(Arrays.toString(row));
+        }
+    }
+
+    @Test
     void minPathSum() {
         int[][] grid = new int[][]{{1,3,1},{1,5,1},{4,2,1}};
         assertEquals(7, new Math().minPathSum(grid));
