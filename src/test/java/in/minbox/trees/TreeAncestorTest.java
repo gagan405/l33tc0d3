08d93107b691837825ffc8b2a5f1aa150f339c01
𓂺 <gagan@minbox.in>
2024-03-02 23:59:28 -0800

    fix star import

--- src/test/java/in/minbox/trees/TreeAncestorTest.java
+++ src/test/java/in/minbox/trees/TreeAncestorTest.java
@@ -2,7 +2,7 @@ package in.minbox.trees;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TreeAncestorTest {
 
